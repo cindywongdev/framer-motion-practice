1850bbdfd18a1749e24a542ce2427c482b09411b
Cindy Wong <cindywongdev@gmail.com>
2023-05-01 17:34:12 -0400

    practice

---
+++ src/components/test.jsx
@@ -0,0 +1,13 @@
+import { motion } from "framer-motion"
+
+const Test = (props) => {
+    return <>
+        <motion.p 
+            animate={{ scale: 2, x: 100 }}
+            transition={{ type: "spring", duration: 1 }}>
+            Hello
+        </motion.p>
+    </>
+}
+
+export default Test
